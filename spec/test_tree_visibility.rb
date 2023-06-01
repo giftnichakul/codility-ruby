dd4ac1267c4085350ea6f98c21bb0cff9ebb7102
Nichakul Pichitwutikorn <Nichakul.p@DW000460.local>
2023-06-01 10:54:55 +0700

    Format code to more readable

--- spec/test_tree_visibility.rb
+++ spec/test_tree_visibility.rb
@@ -2,14 +2,28 @@ require_relative '../solution/tree_visibility'
 
 RSpec.describe 'example test' do
   it 'example1' do
-    t1 = TreeVisibility.new(5,
-                            TreeVisibility.new(3, TreeVisibility.new(20, nil, nil), TreeVisibility.new(21, nil, nil)), TreeVisibility.new(10, TreeVisibility.new(1, nil, nil), nil))
+    t1 = TreeVisibility.new(
+      5,
+      TreeVisibility.new(
+        3,
+        TreeVisibility.new(20, nil, nil),
+        TreeVisibility.new(21, nil, nil)
+      ),
+      TreeVisibility.new(10, TreeVisibility.new(1, nil, nil), nil)
+    )
     expect(t1.solution).to eq(4)
   end
 
   it 'example2' do
-    t2 = TreeVisibility.new(8,
-                            TreeVisibility.new(2, TreeVisibility.new(8, nil, nil), TreeVisibility.new(7, nil, nil)), TreeVisibility.new(6, nil, nil))
+    t2 = TreeVisibility.new(
+      8,
+      TreeVisibility.new(
+        2,
+        TreeVisibility.new(8, nil, nil),
+        TreeVisibility.new(7, nil, nil)
+      ),
+      TreeVisibility.new(6, nil, nil)
+    )
     expect(t2.solution).to eq(2)
   end
 end
